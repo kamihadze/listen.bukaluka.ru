9ecee268cb9e86b0d62bd647318700aa6d3b0add
Михаил Джуган <kamihadze@MacBook-Pro-Mihail-3.local>
2026-03-29 19:32:35 +0300

    FAQ-секция с аккордеоном на странице

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import './styles/index.css'
 import { initNav } from './modules/nav'
 import { initScrollAnimations } from './modules/scroll-animations'
+import { initFaq } from './modules/faq'
 
 initNav()
 initScrollAnimations()
+initFaq()
